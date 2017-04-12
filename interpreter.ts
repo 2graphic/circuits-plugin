88645d71a68cf98116197b2b27d7f66b6ebb7b98
Sheyne Anderson <sheyne@sheyne.com>
2017-04-12 14:06:31 -0600

    update

--- interpreter.ts
+++ interpreter.ts
@@ -6,29 +6,24 @@ export class BasicGate {
     children: Wire[];
     parents: Wire[];
     label: string;
-    protected value: boolean;
-    getValue(): boolean {
-        return this.value;
-    }
-    setValue(value: boolean) {
-        this.value = value;
-    }
+    value: boolean;
 }
 
 export class InputGate extends BasicGate {
+    image: "and_gate.svg";
     inputIndex: number;
 }
 
 export class OutputGate extends BasicGate {
 }
 
+export class NotGate extends BasicGate {
+}
+
 export class AndGate extends BasicGate {
 }
 
 export class OrGate extends BasicGate {
-}
-
-export class NotGate extends BasicGate {
 }
 
 export class Wire {
@@ -69,52 +64,71 @@ function easyReduce<T, R>(arr: T[], func: (current: T, result: R) => R, initial:
     return result;
 }
 
-interface InputType {"a": boolean; "b": boolean; };
-
 export class State {
     message: string;
 
-    constructor(public toVisit: Nodes[], public output: Object, public active: Nodes, public value: boolean, public input: InputType) {
-        this.message = toVisit.map((n) => n.label).join();
+    constructor(public toVisit: Nodes[], public output: Map<OutputGate, boolean>,  public active: Nodes, public value: boolean) {
+        if (toVisit.length > 0) {
+            this.message = toVisit.map((n) => n.label).join();
+        } else {
+            this.message = "finished";
+        }
     }
 }
 
 function applyOp(node: BasicGate, op: (a: boolean, b: boolean) => boolean, init: boolean): boolean {
-    return easyReduce(node.parents, (parent, current) => op(parent.source.getValue(), current), init);
+    return easyReduce(node.parents, (parent, current) => op(parent.source.value, current), init);
 }
 
-export function start(start: Circuit, input: InputType): string | State {
+export function start(start: Circuit, input: Map<InputGate, boolean>): State | Map<OutputGate, boolean> {
+    if (!start.nodes.find((n) => n instanceof InputGate)) {
+        throw new Error("Need at least one InputGate");
+    }
+    if (!start.nodes.find((n) => n instanceof OutputGate)) {
+        throw new Error("Need at least one OutputGate");
+    }
+
+    if (!input) {
+        return new Map();
+    }
+
     const toVisit = getTraversalOrder(start);
-    const active = toVisit[0];
-    active.setValue(input[active.label]);
-    return new State(toVisit.slice(1), {}, active, active.getValue(), input);
+
+    while (toVisit[0] instanceof InputGate) {
+        let active = toVisit.shift();
+        active.value = input.get(active as InputGate);
+    }
+
+    if (toVisit.length > 0) {
+        return new State(toVisit, new Map(), toVisit[0], undefined);
+    } else {
+        throw new Error("Error running plugin.");
+    }
 }
 
-export function step(state: State): Object | State {
+export function step(state: State): State | Map<OutputGate, boolean> {
     if (state.toVisit.length === 0) {
         return state.output;
     } else {
-        let output = state.output;
-        const node: BasicGate = state.toVisit[0];
+        const node: BasicGate = state.toVisit.shift();
         let result: boolean;
 
-        if (node instanceof InputGate) {
-            node.setValue(state.input[node.label]);
-            result = node.getValue();
-        } else if (node instanceof AndGate) {
+        console.log(node);
+
+        if (node instanceof AndGate) {
             result = applyOp(node, (a, b) => a && b, true);
         } else if (node instanceof OrGate) {
             result = applyOp(node, (a, b) => a || b, false);
         } else if (node instanceof NotGate) {
-            result = !node.parents[0].source.getValue();
+            result = !node.parents[0].source.value;
         } else if (node instanceof OutputGate) {
-            result = node.parents[0].source.getValue();
-            output[node.label] = result;
+            result = node.parents[0].source.value;
+            state.output.set(node, result);
         } else {
             throw new Error(`Unknown type of node: ${Object.getPrototypeOf(node)}`);
         }
 
-        node.setValue(result);
-        return new State(state.toVisit.slice(1), output, node, node.getValue(), state.input);
+        node.value = result;
+        return new State(state.toVisit, state.output, node, node.value);
     }
 }
